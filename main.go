b2b3a882cef3c4ab185e8cf05407f2cac9a48591
tartan <tartanec@gmail.com>
2024-05-02 21:36:05 +0200

    02.07

--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello from Snippetbox"))
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
 // Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a form for creating a new snippet..."))
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Display a form for creating a new snippet..."))
+
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request){
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
 	w.Write([]byte("creating snippet"))
+
 }
 
 func main() {
-    mux := http.NewServeMux()
-    // Prefix the route patterns with the required HTTP method (for now, we will
-    // restrict all three routes to acting on GET requests).
-    mux.HandleFunc("GET /{$}", home)
-    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux := http.NewServeMux()
+	// Prefix the route patterns with the required HTTP method (for now, we will
+	// restrict all three routes to acting on GET requests).
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Print("starting server on :4000")
+	log.Print("starting server on :4000")
 
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
