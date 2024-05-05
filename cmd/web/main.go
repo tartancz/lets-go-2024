d3474fe9f092ecfb64927d9182e60fed45c04159
tartan <tartanec@gmail.com>
2024-05-05 23:14:44 +0200

    03.04

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,39 +1,44 @@
 package main
 
 import (
-	"flag"
-	"log/slog"
-	"net/http"
-	"os"
+    "flag"
+    "log/slog"
+    "net/http"
+    "os"
 )
 
+type application struct {
+    logger *slog.Logger
+}
+
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    flag.Parse()
 
-	// Use the slog.New() function to initialize a new structured logger, which
-	// writes to the standard out stream and uses the default settings.
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+    // Initialize a new instance of our application struct, containing the
+    // dependencies (for now, just the structured logger).
+    app := &application{
+        logger: logger,
+    }
 
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+    
+    // Swap the route declarations to use the application struct's methods as the
+    // handler functions.
+    mux.HandleFunc("GET /{$}", app.home)
+    mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+    mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+    mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+    
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	// Use the Info() method to log the starting server message at Info severity
-	// (along with the listen address as an attribute).
-	logger.Info("starting server", "addr", *addr)
-
-	err := http.ListenAndServe(*addr, mux)
-	// And we also use the Error() method to log any error message returned by
-	// http.ListenAndServe() at Error severity (with no additional attributes),
-	// and then call os.Exit(1) to terminate the application with exit code 1.
-	logger.Error(err.Error())
-	os.Exit(1)
+    logger.Info("starting server", "addr", *addr)
+    
+    err := http.ListenAndServe(*addr, mux)
+    logger.Error(err.Error())
+    os.Exit(1)
 }
