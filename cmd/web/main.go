d17377e4b8177c9bdd6b0773d6bda2be4ec7655e
tartan <tartanec@gmail.com>
2024-05-06 19:21:56 +0200

    03.06

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,44 +1,31 @@
 package main
 
 import (
-    "flag"
-    "log/slog"
-    "net/http"
-    "os"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
 )
 
 type application struct {
-    logger *slog.Logger
+	logger *slog.Logger
 }
 
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
-    flag.Parse()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-    // Initialize a new instance of our application struct, containing the
-    // dependencies (for now, just the structured logger).
-    app := &application{
-        logger: logger,
-    }
+	// Initialize a new instance of our application struct, containing the
+	// dependencies (for now, just the structured logger).
+	app := &application{
+		logger: logger,
+	}
 
-    mux := http.NewServeMux()
+	logger.Info("starting server", "addr", *addr)
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-    
-    // Swap the route declarations to use the application struct's methods as the
-    // handler functions.
-    mux.HandleFunc("GET /{$}", app.home)
-    mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-    mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-    mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-    
-
-    logger.Info("starting server", "addr", *addr)
-    
-    err := http.ListenAndServe(*addr, mux)
-    logger.Error(err.Error())
-    os.Exit(1)
+	err := http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
 }
