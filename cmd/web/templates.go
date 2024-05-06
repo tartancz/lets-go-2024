a96fd167f6ffd24c9ce596ca01cd6b0939f4996b
tartan <tartanec@gmail.com>
2024-05-06 23:11:33 +0200

    06.00

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -25,6 +25,17 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 	}
 }
 
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+    "humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -37,7 +48,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		name := filepath.Base(page)
 
 		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
